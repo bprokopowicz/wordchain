ac30d01097d9d9b2707916112f8a9e69f98430bf
Bonnie Prokopowicz <bonnie@bonnies-air.lan>
2025-02-07 20:32:43 -0600

    Add left margin to game-div; fix dictionary size test

--- test/Test.js
+++ test/Test.js
@@ -439,7 +439,7 @@ class Test extends BaseLogger {
         this.testName = "DictFull";
 
         const dictSize = this.fullDict.getSize();
-        const expectedMinDictSize = 15974;
+        const expectedMinDictSize = 15972;
 
         const catAdders = this.fullDict.findAdderWords("CAT");
         const addersSize = catAdders.size;
