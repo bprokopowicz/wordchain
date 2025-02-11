bd3793d092984ac60386268ec5babe9c149dd2a8
Bonnie Prokopowicz <bonnie@bonnies-air.lan>
2025-02-11 13:14:58 -0600

    turn of obfuscation in webpack-config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,12 @@ module.exports = {
     output: {
         filename: 'bundle.js'
     },
+    /*
+    uncomment this to apply obfuscation.  As of Feb 11, 2025, obfuscation
+    makes the client-side app unreliable or completely unusable, due to 
+    runtime undefined references.
     plugins: [
-        new WebpackObfuscator({
-             rotateStringsArray: true
-        }, ['js-file-to-exclude.js'])
+        new WebpackObfuscator({ }, ['js-file-to-exclude.js'])
     ]
+    */
 };
