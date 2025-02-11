62e4c4f89a65acede3a8e7b8f28720152c55d94a
Pete Prokopowicz <pete.prokopowicz@gmail.com>
2025-02-11 11:52:07 -0600

    adding webpack and obfuscation support

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+var WebpackObfuscator = require('webpack-obfuscator');
 module.exports = {
     entry: {
         main: './docs/javascript/AppDisplay.js'
-   },
+    },
+    output: {
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new WebpackObfuscator({
+             rotateStringsArray: true
+        }, ['js-file-to-exclude.js'])
+    ]
 };
