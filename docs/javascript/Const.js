8890725252513a08fe2f2048143b3f7f2139c993
Bonnie Prokopowicz <bonnie.prokopowicz@gmail.com>
2025-10-18 15:37:30 -0500

    help changes for beta deployment at github pages

--- docs/javascript/Const.js
+++ docs/javascript/Const.js
@@ -20,6 +20,7 @@ export function GET_GL_DEBUG(value) {
 export const SHARE_URL         = "https://WordChainFun.com/"; // The real URL
 export const DICT_URL          = "https:/wordchain/docs/resources/WordChainDict";
 export const SCRABBLE_DICT_URL = "https:/wordchain/docs/resources/Scrabble3-6";
+export const HELP_IMAGE_URL    = "https:/wordchain/docs/images";
 export const EMAIL_URL         = "mailto:wordchainfun@gmail.com?subject=WordChain%20Feedback";
 export const FAQ_URL           = "/wordchain/FAQ.html";
 export const HELP_VIDEO_URL    = "https://youtu.be/Tu5V7CyclfQ?si=vDWfMszYZEV6bsLP";
